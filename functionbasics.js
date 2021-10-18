d90a6ac6c6583ca43942d0e661d0a09e1f83dd8a
joery12 <90188965+joery12@users.noreply.github.com>
2021-10-18 17:49:17 +0200

    Add files via upload

---
+++ functionbasics.js
@@ -0,0 +1,21 @@
+const makePizza = function() {
+    console.log("Make dough.");
+    console.log("Add ingredients on top of dough.");
+    console.log("Put the pizza in the oven.");
+    console.log("Take out the pizza when heated.");
+};
+
+const makeSushi = function() {
+    console.log("Clean fish");
+    console.log("Cut fish in small parts");
+    console.log("Place the seaweed in the bamboo roll");
+    console.log("Add rice to the roll");
+    console.log("Add the parts of fish to the roll");
+    console.log("Cut the roll in sushi parts");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
